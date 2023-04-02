3450853e325f64b890b53420286aa2d9458239d6
Aleksz <aleksznoob@gmail.com>
2023-04-02 14:54:22 +0200

    make welcome view controller

---
+++ Lesson 2.6/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  Lesson 2.6
+//
+//  Created by Иван on 02.04.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    // MARK: - Private constans
+    var userName = ""
+    var password = ""
+    
+    // MARK: - Private @IBOutlet
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(userName)"
+    }
+    
+    @IBAction func logOutButton(_ sender: Any) {
+        performSegue(withIdentifier: "WelcomeBackButton", sender: self)
+    }
+}
