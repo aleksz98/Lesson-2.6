f537773ba1cdb506385e8fa749b72e430db587da
Aleksz <aleksznoob@gmail.com>
2023-04-05 17:36:29 +0200

    made background welcomeVC

--- Lesson 2.6/WelcomeViewController.swift
+++ Lesson 2.6/WelcomeViewController.swift
@@ -12,6 +12,19 @@ final class WelcomeViewController: UIViewController {
     var userName = ""
     var password = ""
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
+    
     // MARK: - Private @IBOutlet
     @IBOutlet weak var welcomeLabel: UILabel!
     
@@ -19,6 +32,20 @@ final class WelcomeViewController: UIViewController {
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         welcomeLabel.text = "Welcome, \(userName)"
+    }
+}
+
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
